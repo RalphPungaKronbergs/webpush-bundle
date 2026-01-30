8034aa9896c868333bd12dd235b7a6dfe0bf0c05
Ralph Punga-Kronbergs <github@punga.de>
2026-01-30 17:58:25 +0100

    Replace XmlFileLoader with PhpFileLoader in WebPushExtension

--- src/DependencyInjection/WebPushExtension.php
+++ src/DependencyInjection/WebPushExtension.php
@@ -4,19 +4,11 @@ namespace BenTools\WebPushBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Validator\Mapping\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
- */
 class WebPushExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
@@ -25,13 +17,11 @@ class WebPushExtension extends Extension
         $container->setParameter('bentools_webpush.vapid_subject', $config['settings']['subject'] ?? $container->getParameter('router.request_context.host'));
         $container->setParameter('bentools_webpush.vapid_public_key', $config['settings']['public_key'] ?? null);
         $container->setParameter('bentools_webpush.vapid_private_key', $config['settings']['private_key'] ?? null);
-        $loader = new XmlFileLoader($container, new FileLocator([__DIR__.'/../Resources/config/']));
-        $loader->load('services.xml');
+        
+        $loader = new PhpFileLoader($container, new FileLocator([__DIR__.'/../Resources/config/']));
+        $loader->load('services.php');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getAlias(): string
     {
         return 'bentools_webpush';
